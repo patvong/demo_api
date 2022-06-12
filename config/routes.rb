824c4fb48fd27443aa9ff24e7d36dda008091838
Pat Vongphrachanh <p.vongphrachanh@gmail.com>
2022-06-12 15:55:13 -0400

    feat: create namespace and versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, param: :sku
+  namespace :api do
+    namespace :v1 do
+      resources :products, param: :sku
+    end
+  end
+
 end
